ce7b28c458e56d1b824f5cc7bea61a5843169b83
Kevin Macaulay <kevin@softwareops.com>
2017-07-13 11:17:15 -0700

    Modifying close button.

--- TagListView/CloseButton.swift
+++ TagListView/CloseButton.swift
@@ -23,18 +23,24 @@ internal class CloseButton: UIButton {
         path.lineCapStyle = .round
 
         let iconFrame = CGRect(
-            x: (rect.width - iconSize) / 2.0,
-            y: (rect.height - iconSize) / 2.0,
-            width: iconSize,
-            height: iconSize
+            x: (rect.width - iconSize) / 2.0 + 2,
+            y: (rect.height - iconSize) / 2.0 + 2,
+            width: iconSize - 4,
+            height: iconSize - 4
         )
+        
+        path.addArc(withCenter: CGPoint(x: iconFrame.midX, y: iconFrame.midY), radius: iconSize, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: true)
+        
+        lineColor.setStroke()
+        
+        path.stroke()
 
         path.move(to: iconFrame.origin)
         path.addLine(to: CGPoint(x: iconFrame.maxX, y: iconFrame.maxY))
         path.move(to: CGPoint(x: iconFrame.maxX, y: iconFrame.minY))
         path.addLine(to: CGPoint(x: iconFrame.minX, y: iconFrame.maxY))
 
-        lineColor.setStroke()
+        UIColor.white.setStroke()
 
         path.stroke()
     }
